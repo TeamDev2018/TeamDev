d06e57a4d60fc19e702df8e927c196a34864a673
Martin <martin@sap-awe.de>
2018-10-21 15:47:17 +0200

    added route to user-detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TeamOverviewComponent } from './team-overview/team-overview.component';
 import { TeamComponent } from './team/team.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 const routes: Routes = [
   { path: 'team-overview', component: TeamOverviewComponent },
   { path: 'team', component: TeamComponent },
+  { path: 'user-detail/:id', component: UserDetailComponent },
   { path: '', redirectTo: '/team-overview', pathMatch: 'full' }
 ];
 
