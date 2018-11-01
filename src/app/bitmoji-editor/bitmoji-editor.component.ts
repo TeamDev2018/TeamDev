b263f048d510b521659acbc763cc503dd6583c39
Martin <martin@sap-awe.de>
2018-11-01 15:24:56 +0100

    Complete (ugly) editor

--- src/app/bitmoji-editor/bitmoji-editor.component.ts
+++ src/app/bitmoji-editor/bitmoji-editor.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { buildPreviewUrl, Traits, genders, styles, randTraits, randOutfit,
-  randInt, getTraits, getBrands, getOutfits, randBrand } from 'libmoji';
+import { Traits, genders, styles, randTraits, randOutfit,
+  randInt, getTraits, getBrands, getOutfits, randBrand, poses } from 'libmoji';
 
 @Component({
   selector: 'app-bitmoji-editor',
@@ -19,6 +19,7 @@ export class BitmojiEditorComponent implements OnInit {
   brand;
 
   listTraits: Traits[];
+  listPoses: [];
   listGenders: [];
   listStyles: [];
   listOutfits: [];
@@ -27,18 +28,53 @@ export class BitmojiEditorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.pose = 'head';
+    this.listPoses = poses;
+    if (!this.pose) {
+      this.pose = 'fashion';
+    }
+    if (!this.scale) {
+      this.scale = 2;
+    }
+    if (!this.gender) {
+      this.gender = randInt(2);
+    }
+    if (!this.style) {
+      this.style = randInt(3);
+    }
+    this.listTraits = getTraits(genders[this.gender][0], styles[this.style][0]);
+    debugger;
+    if (!this.traits) {
+      this.traits = randTraits(this.listTraits);
+    }
+
+    this.listBrands = getBrands(genders[this.gender][0]);
+    if (!this.brand) {
+      this.brand = randInt(this.listBrands.length);
+    }
+    this.listOutfits = getOutfits(this.listBrands[this.brand]);
+    if (!this.outfit) {
+      this.outfit = randOutfit(this.listOutfits);
+    }
+    if (!this.rotation) {
+      this.rotation = 0;
+    }
     this.listGenders = genders;
     this.listStyles = styles;
-    // debugger;
 
-    // this.listOutfits = getOutfits(this.brand);
-
-    // this.listTraits = getTraits();
+  }
+  brandChanged() {
+    this.listOutfits = getOutfits(this.listBrands[this.brand]);
+    this.outfit = randOutfit(this.listOutfits);
+  }
+  styleChanged() {
+    this.listTraits = getTraits(genders[this.gender][0], styles[this.style][0]);
+    this.traits = randTraits(this.listTraits);
   }
   genderChanged() {
     this.listBrands = getBrands(genders[this.gender][0]);
-    // this.brand = this.listBrands[0];
+    this.brand = randBrand(this.listBrands);
+    this.listOutfits = getOutfits(this.brand);
+    this.outfit = randOutfit(this.listOutfits);
   }
 
 }
