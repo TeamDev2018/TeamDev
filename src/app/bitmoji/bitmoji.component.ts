ceaceb776656a49c42bf1f58576989dda1c58400
Martin <martin@sap-awe.de>
2018-11-01 13:52:17 +0100

    randomize not provided features

--- src/app/bitmoji/bitmoji.component.ts
+++ src/app/bitmoji/bitmoji.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { buildPreviewUrl, Traits } from 'libmoji';
+import { buildPreviewUrl, Traits, genders, styles, randTraits, randOutfit,
+  randInt, getTraits, getBrands, getOutfits, randBrand } from 'libmoji';
 
 @Component({
   selector: 'app-bitmoji',
@@ -19,7 +20,30 @@ export class BitmojiComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.url = buildPreviewUrl(this.pose, this.scale, this.gender, this.style, this.rotation, this.traits, this.outfit);
+    if (!this.pose) {
+      this.pose = 'fashion';
+    }
+    if (!this.scale) {
+      this.scale = 3;
+    }
+    if (!this.gender) {
+      this.gender = randInt(2);
+    }
+    if (!this.style) {
+      this.style = randInt(3);
+    }
+    if (!this.traits) {
+      this.traits = randTraits(getTraits(genders[this.gender][0], styles[this.style][0]));
+    }
+    if (!this.outfit) {
+      this.outfit = randOutfit(getOutfits(randBrand(getBrands(genders[this.gender][0]))));
+    }
+    if (!this.rotation) {
+      this.rotation = 0;
+    }
+
+    // tslint:disable-next-line:max-line-length
+    this.url = buildPreviewUrl(this.pose, this.scale, genders[this.gender][1], styles[this.style][1], this.rotation, this.traits, this.outfit);
   }
 
 }
