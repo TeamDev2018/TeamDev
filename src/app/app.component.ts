16f81e10c8317a9359277bb2fdb240a77f199b5f
Cloud925 <lukas_anetsberger@web.de>
2018-11-12 21:49:36 +0100

    refactor animations -> animations.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { routeChange } from './animations/animations';
 import { animate, query, sequence, style, transition, trigger, group } from '@angular/animations';
 import { NewUserDialogComponent } from './new-user-dialog/new-user-dialog.component';
 import { USERS } from '../data/users';
@@ -6,28 +7,13 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
-const fadeIn = [
-  query(':leave, :enter', style({ position: 'fixed', height: '100%', width: '100%' }), { optional: true }),
-  query(':enter', style({ visibility: 'hidden' })),
-  sequence([
-    query(':leave', [
-      style({ overflow: 'hidden', height: '100%' }),
-      animate('0.5s ease-in-out', style({ height: '0%' })),
-    ], { optional: true }),
-    query(':enter', [
-      style({ visibility: 'visible', overflow: 'hidden', height: '0%' }),
-      animate('0.5s ease-in-out', style({ height: '100%' }))
-    ], { optional: true })
-  ])
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
     trigger('routeAnimation', [
-      transition('teamOverview => teamList', fadeIn)
+      transition('teamOverview <=> teamList', routeChange)
     ])
   ]
 })
