18f28539ae44c5dbb397757deb04a519d704d8c0
Martin <martin@sap-awe.de>
2018-11-12 21:02:38 +0100

    Fix junit reporting / wrong folder

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -8,9 +8,9 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-junit-reporter'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
