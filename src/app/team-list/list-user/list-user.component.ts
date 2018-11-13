2dbe2ef13592871e66b380e8c3db2711897a8322
Martin <martin@sap-awe.de>
2018-11-13 10:24:38 +0100

    remove obsolete interceptor

--- src/app/team-list/list-user/list-user.component.ts
+++ src/app/team-list/list-user/list-user.component.ts
@@ -21,27 +21,23 @@ import { Component, ElementRef, HostListener, Input, OnInit } from '@angular/cor
 })
 export class ListUserComponent implements OnInit {
   private state: String = 'inactive';
-  @Input() intersector: any;
   @Input() user: User;
 
   constructor(public el: ElementRef) { }
 
   ngOnInit() {
-    if (this.intersector) {
-      const _observer = new IntersectionObserver( (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
-        const entry = entries[0];
-        console.log(entry, observer);
-        if (entry.isIntersecting) {
-          this.state = 'active';
-        } else {
-          this.state = 'inactive';
-        }
-      }, {
-        root: null,
-        rootMargin: '-49%'
-      });
-      _observer.observe(this.el.nativeElement);
-    }
+    const _observer = new IntersectionObserver( (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      const entry = entries[0];
+      console.log(entry, observer);
+      if (entry.isIntersecting) {
+        this.state = 'active';
+      } else {
+        this.state = 'inactive';
+      }
+    }, {
+      root: null,
+      rootMargin: '-49%'
+    });
+    _observer.observe(this.el.nativeElement);
   }
-
 }
